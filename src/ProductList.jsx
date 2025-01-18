cb5af0f0563a598b1228766519246c5449f90193
TagRakan <rakantoss@gmail.com>
2025-01-18 12:29:34 -0500

    Additional Features

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -10,6 +10,7 @@ function ProductList() {
     const cartItems = useSelector((state) => state.cart);
     const dispatch = useDispatch();
 
+    
 
     const plantsArray = [
         {
@@ -248,16 +249,39 @@ const handlePlantsClick = (e) => {
     setShowCart(false); // Hide the cart when navigating to About Us
 };
 
+const isItemAdded = (n) => {
+    const existed = cartItems.items.find(i => i.name === n);
+    if (existed){
+        return true;
+    } else {
+        return false;
+    }
+};
+
    const handleContinueShopping = (e) => {
     e.preventDefault();
     setShowCart(false);
   };
+
+
+  const getTotal = () => {
+    let aTotal = 0;
+    cartItems.items.forEach( (i) => {
+        aTotal += i.quantity;
+    });
+return aTotal;
+  };
+  const cartTotal = getTotal();
+
   const handleAddToCart = (p) => {
     dispatch(addItem(p));
     setAddedToCart((prev) => ({
         ...prev, [p.name]: true,
-    }));
+    })); 
+
   };
+
+
     return (
         <div>
              <div className="navbar" style={styleObj}>
@@ -275,7 +299,8 @@ const handlePlantsClick = (e) => {
             </div>
             <div style={styleObjUl}>
                 <div> <a href="#" onClick={(e)=>handlePlantsClick(e)} style={styleA}>Plants</a></div>
-                <div>123 <a href="#" onClick={(e) => handleCartClick(e)} style={styleA}><h1 className='cart'><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68"><rect width="156" height="156" fill="none"></rect><circle cx="80" cy="216" r="12"></circle><circle cx="184" cy="216" r="12"></circle><path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" id="mainIconPathAttribute"></path></svg></h1></a></div>
+                <div> {cartTotal} <a href="#" onClick={(e) => handleCartClick(e)} style={styleA}><h1 className='cart'><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68"><rect width="156" height="156" fill="none"></rect><circle cx="80" cy="216" r="12"></circle><circle cx="184" cy="216" r="12"></circle><path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" id="mainIconPathAttribute"></path></svg></h1></a></div>
+                
             </div>
         </div>
         {!showCart? (
@@ -290,7 +315,10 @@ const handlePlantsClick = (e) => {
                             <div className="product-title">{plant.name}</div>
                             <div className='product-price'>{plant.cost}</div>
                             <p>{plant.description}</p>
+                            {isItemAdded(plant.name) ? (<button  className="product-button added-to-cart">Added to Cart</button>) 
+                            : (
                             <button  className="product-button" onClick={() => handleAddToCart(plant)}>Add to Cart</button>
+                            ) }
                         </div>
                         ))}
                     </div>
