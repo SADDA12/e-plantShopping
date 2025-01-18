7da2742f163159a0ebc788bd8c50162123eb3b36
TagRakan <rakantoss@gmail.com>
2025-01-18 08:52:19 -0500

    Two tasks done

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -7,11 +7,23 @@ export const CartSlice = createSlice({
   },
   reducers: {
     addItem: (state, action) => {
-    
+        const {name,image,cost} = action.payload;
+        const existed = state.items.find(i => i.name === name);
+        if (existed){
+            existed.quantity++;
+        } else {
+            state.items.push({name,image,cost, quantity: 1});
+        }
     },
     removeItem: (state, action) => {
+        state.items = state.items.filter(i => i.name !== i.payload);
     },
     updateQuantity: (state, action) => {
+        const { name, quantity } = action.payload;
+        const targetedItem = state.items.find(i => i.name === name);
+        if (targetedItem) {
+            targetedItem.quantity = quantity;
+        }
 
     
     },
